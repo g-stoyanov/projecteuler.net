a6c62645c63266ddb075cefe32873536032aeac8
g-stoyanov <georgi.j.stoyanov@gmail.com>
2015-01-22 17:28:06 +0200

    Added solution for problem 10

---
+++ ProjectEulerDotNetProblems/ProjectEulerDotNetProblems/From0To100/Problem010SummationOfPrimes.cs
@@ -0,0 +1,30 @@
+﻿namespace ProjectEulerDotNetProblems
+{
+    using ProjectEulerDotNetProblems.Utilities;
+
+    public static class Problem010SummationOfPrimes
+    {
+        /// <summary>
+        /// Summation of primes
+        /// ...................
+        /// Problem 10
+        /// ----------
+        /// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+        /// 
+        /// Find the sum of all the primes below two million.
+        /// </summary>
+        public static ulong CalculateSumOfAllPrimesBellowGivenBorder(uint border)
+        {
+            ulong sumOfAllPrimes = 2;
+            for (ulong primeCandidate = 3; primeCandidate < border; primeCandidate += 2)
+            {
+                if (MathematicsUtilities.IsPrime(primeCandidate))
+                {
+                    sumOfAllPrimes += primeCandidate;
+                }
+            }
+
+            return sumOfAllPrimes;
+        }
+    }
+}
