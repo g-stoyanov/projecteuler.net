ca30ca572def9ee52ad9e24333fc8e969567dcc2
g-stoyanov <georgi.j.stoyanov@gmail.com>
2015-01-22 07:26:15 +0200

    Add files

---
+++ ProjectEulerDotNetProblems/ProjectEulerDotNetProblems/From0To100/Problem003LargestPrimeFactor.cs
@@ -0,0 +1,34 @@
+﻿namespace ProjectEulerDotNetProblems.From0To100
+{
+    using ProjectEulerDotNetProblems.Utilities;
+
+    public static class Problem003LargestPrimeFactor
+    {
+        /// <summary>
+        /// Largest prime factor
+        /// ....................
+        /// Problem 3
+        /// ---------
+        /// The prime factors of 13195 are 5, 7, 13 and 29.
+        ///
+        /// What is the largest prime factor of the number 600851475143 ?
+        /// </summary>
+        public static ulong CalculateLargestPrimeFactorOfTheNumber(ulong number)
+        {
+            if ((number & 1) == 0)
+            {
+                number -= 1;
+            }
+
+            for (ulong candidate = number; candidate >= 0; candidate -= 2)
+            {
+                if (MathematicsUtilities.IsFactor(number, candidate) && MathematicsUtilities.IsPrime(candidate))
+                {
+                    return candidate;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ ProjectEulerDotNetProblems/ProjectEulerDotNetProblems/Utilities/MathematicsUtilities.cs
@@ -0,0 +1,51 @@
+﻿namespace ProjectEulerDotNetProblems.Utilities
+{
+    using System;
+
+    public static class MathematicsUtilities
+    {
+        public static bool IsPrime(ulong number)
+        {
+            bool result = true;
+            double border = Math.Sqrt(number);
+            for (ulong num = 2; num <= border; num++)
+            {
+                if (number % num == 0)
+                {
+                    return false;
+                }
+            }
+
+            return result;
+        }
+
+        public static bool IsFactor(ulong number, ulong candidate)
+        {
+            if (number % candidate == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsPalindrome(ulong number)
+        {
+            ulong numberToTest = number;
+            ulong reversedNumber = 0;
+            while (number > 0)
+            {
+                ulong digNumber = number % 10;
+                reversedNumber = reversedNumber * 10 + digNumber;
+                number = number / 10;
+            }
+
+            if (numberToTest == reversedNumber)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
