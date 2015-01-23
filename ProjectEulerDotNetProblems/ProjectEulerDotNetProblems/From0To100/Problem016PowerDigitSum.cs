2dc69144d416bdb17334d7b3b4ea8b5e75c9fc88
g-stoyanov <georgi.j.stoyanov@gmail.com>
2015-01-23 17:39:30 +0200

    Added solution for problem 16

---
+++ ProjectEulerDotNetProblems/ProjectEulerDotNetProblems/From0To100/Problem016PowerDigitSum.cs
@@ -0,0 +1,36 @@
+﻿namespace ProjectEulerDotNetProblems.From0To100
+{
+    using System;
+    using System.Numerics;
+
+    public static class Problem016PowerDigitSum
+    {
+        /// <summary>
+        /// Power digit sum
+        /// ...............
+        /// Problem 16
+        /// ----------
+        /// 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+        /// 
+        /// What is the sum of the digits of the number 21000?
+        /// </summary>
+        public static ulong CalculateDigitsSumOfPoweredNum(int number, int power)
+        {
+            BigInteger poweredNumber = 1;
+            for (int powerCount = 0; powerCount < power; powerCount++)
+            {
+                poweredNumber *= number;
+            }
+
+            string poweredNumberDigits = poweredNumber.ToString();
+
+            ulong sum = 0;
+            foreach (var digit in poweredNumberDigits)
+            {
+                sum += (ulong)Char.GetNumericValue(digit);
+            }
+
+            return sum;
+        }
+    }
+}
